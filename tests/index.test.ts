cb4487a249c6d574955b5deaa9a58a46a2b6c7c9
Diego Cunha <dc811cunha@gmail.com>
2024-10-16 21:55:17 -0300

    escopo

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,6 +2,12 @@ import { ext } from '../src/index';
 
 describe('numeroPorExtenso', () => {
     test('deve retornar o número por extenso corretamente', () => {
-        expect(ext(1)).toBe('zero');
+        expect(ext(10)).toBe("");
+    });
+});
+
+describe('numeroPorExtenso', () => {
+    test('deve retornar o null se fora do escopo', () => {
+        expect(ext(-1)).toBe("");
     });
 });
